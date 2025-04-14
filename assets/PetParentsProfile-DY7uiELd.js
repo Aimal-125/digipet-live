21921845c9d76913e0554c85630d514c03635632
Aimal Khan <aimal4910@gmail.com>
2025-04-14 16:52:52 +0500

    updated admin-panel-live

---
+++ assets/PetParentsProfile-DY7uiELd.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-aNOJxs4N.js";function t(){return e.jsx(e.Fragment,{children:e.jsx("div",{children:"Pet Parents Profile Page"})})}export{t as default};
---
+++ assets/UpdatePasswrod-2URuCy9M.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-aNOJxs4N.js";function a(){return e.jsx(e.Fragment,{children:e.jsx("div",{children:"Update Password Page"})})}export{a as default};
