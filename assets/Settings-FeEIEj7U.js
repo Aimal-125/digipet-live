f2a268a2a7e3bc955d0d80263f3aa313818c54c3
Aimal Khan <aimal4910@gmail.com>
2025-04-17 17:09:38 +0500

    updated

---
+++ assets/Settings-FeEIEj7U.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-CbpxotYU.js";function n(){return t.jsx(t.Fragment,{children:t.jsx("div",{children:"Settings Page"})})}export{n as default};
---
+++ assets/UpdatePasswrod-O9T1oJdW.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-CbpxotYU.js";function a(){return e.jsx(e.Fragment,{children:e.jsx("div",{children:"Update Password Page"})})}export{a as default};
