a5e111aaa276fd57903b23d30fbef4c3d4523c84
Aimal Khan <aimal4910@gmail.com>
2025-06-19 19:09:38 +0500

    updated

---
+++ assets/BackButton-B3FMZxYv.js
@@ -0,0 +1,1 @@
+import{u as e,j as n}from"./index-CtF5NqjV.js";import{I as r}from"./index-R1RIAz4U.js";function i(){const s=e();return n.jsx(n.Fragment,{children:n.jsx("div",{children:n.jsxs("button",{type:"button",className:"inline-flex items-center gap-1 cursor-pointer",onClick:()=>{s(-1)},children:[n.jsx("span",{children:n.jsx(r,{})}),n.jsx("span",{children:"Back"})]})})})}export{i as B};
