c05d55bebbf94560d34ff3344cc17e72a97c3247
Aimal Khan <aimal4910@gmail.com>
2025-06-24 22:01:28 +0500

    updated

---
+++ assets/ButtonClipLoader-y6fN6nsg.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-DqT0A0WV.js";function e(){return r.jsx(r.Fragment,{children:r.jsx("div",{className:"loader"})})}export{e as B};
---
+++ assets/apiEndpoints-Bx0RnfJ4.js
@@ -0,0 +1,1 @@
+const t="/appointment",s="/booked-lab-test",o="/pet",A="/speciality",P="/offered-lab-test",_="/doctor";export{t as A,s as B,_ as D,P as O,o as P,A as S};
